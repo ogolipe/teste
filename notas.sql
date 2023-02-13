d46cc7d5f220503350bef6ea8dd0b8eadd447f9b
Luiz Felipe <112408111+LuizMatt97@users.noreply.github.com>
2023-02-13 19:04:27 -0300

    Update notas.sql

--- notas.sql
+++ notas.sql
@@ -1,6 +1,6 @@
 create database crudnotas;
 create table notas(
-	id int primary key auto_increment not null,
+    id int primary key auto_increment not null,
     name varchar(45) not null,
     titulo varchar(45) not null,
     anotacao varchar(255) not null
