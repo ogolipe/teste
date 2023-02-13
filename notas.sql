e8c061d6e24dfb9ae3f8333c4f7d172fbdfefb4f
LuizMatt97 <luiz.fsouzacontato@gmail.com>
2023-02-13 19:03:05 -0300

    banco

---
+++ notas.sql
@@ -0,0 +1,7 @@
+create database crudnotas;
+create table notas(
+	id int primary key auto_increment not null,
+    name varchar(45) not null,
+    titulo varchar(45) not null,
+    anotacao varchar(255) not null
+);
